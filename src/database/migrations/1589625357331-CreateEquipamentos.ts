d2cf1a7c95e687e19d8ccc8f2c04ac50e283cc42
Vinicios Gomes <viniciusgomesviana@gmail.com>
2020-05-16 07:40:03 -0300

    Create connection to database, add migrations users/equipamentos

---
+++ src/database/migrations/1589625357331-CreateEquipamentos.ts
@@ -0,0 +1,80 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateEquipamentos1589625357331
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'equipamentos',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'patrimonio',
+            type: 'varchar',
+          },
+          {
+            name: 'garantia',
+            type: 'varchar',
+          },
+          {
+            name: 'serial_number',
+            type: 'varchar',
+          },
+          {
+            name: 'tipo_item',
+            type: 'varchar',
+          },
+          {
+            name: 'modelo',
+            type: 'varchar',
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+          },
+          {
+            name: 'entidade',
+            type: 'varchar',
+          },
+          {
+            name: 'sinal',
+            type: 'varchar',
+          },
+          {
+            name: 'id_item',
+            type: 'varchar',
+          },
+          {
+            name: 'obs',
+            type: 'varchar',
+          },
+
+          {
+            name: 'fornecedor',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('equipamentos');
+  }
+}
